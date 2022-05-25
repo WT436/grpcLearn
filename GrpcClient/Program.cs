2b2cc24c0a40813f0f69c07196fc6ce3c2eaf8ef
WT436 <WT436.developer@gmail.com>
2022-05-25 21:33:09 +0700

    B1 : create base

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // call server
+            var input = new UserRequest();
+            input.UserName = "Hai Nam";
+            input.Password = "WT436";
+            var connect = GrpcChannel.ForAddress("https://localhost:5001");
+            var sendData = new User.UserClient(connect);
+            var data = await sendData.LoginAsync(input);
+
+            Console.WriteLine("Hello World!" + data);
+        }
+    }
+}
---
+++ GrpcServer/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class UserService : User.UserBase
+    {
+        public override async Task<UserReply> Login(UserRequest request, ServerCallContext context)
+        {
+            UserReply userReply = new UserReply();
+            userReply.UserName = request.UserName;
+            userReply.Password = request.Password;
+            userReply.Token = request.UserName + "|" + request.Password;
+
+            return await Task.FromResult(userReply);
+        }
+    }
+}
